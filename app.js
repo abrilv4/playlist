2508d33e39e0d9c1ad286d96a0f61400fc5a65e5
JhonnyHerbas <jerbasjhon@gmail.com>
2022-04-13 21:52:14 -0400

    Correcciones Primer Sprint

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 $(function () {
     let idactual;
-    let totalmusicas=0
+    let totalmusicas = 0
     console.log('jQuery esta funcionando');
     actualizar();
-    function actualizar(){
+    function actualizar() {
         $.ajax({
             url: 'listamusica.php',
             type: 'GET',
             success: function (response) {
                 let musicas = JSON.parse(response);
-                totalmusicas=musicas.length;
+                totalmusicas = musicas.length;
                 let template = '';
                 musicas.forEach(musica => {
                     template += `
                         <br><table class="container-musica" idMusica="${musica.ID_M}">
                             <tr>
                                 <td class='id'>${musica.ID_M}</td>
+                                <td class='plays'>
+                                    <button class="lista">
+                                        <img class="play" src="play.png">
+                                    </button>
+                                </td>
                                 <td class='nombre'>${musica.NOMBRE_M}</td>
                                 <td class='autor'>${musica.AUTOR_M}</td>
                                 <td class='genero'>${musica.CATEGORIA_M}</td>
                                 <td class='fechaPublicacion'>${musica.FECHAPUBLICACION_M}</td>
                                 <td class='tiempo'>${musica.TIEMPO_M}</td>
-                                <td>
-                                    <button class="lista">
-                                        <img class="play" src="play.png">
-                                    </button>
-                                </td>
                             </tr>
                         </table><br>
                     `
@@ -34,47 +34,47 @@ $(function () {
             }
         })
     }
-    
-    $(document).on('click', '.lista', function(){
+
+    $(document).on('click', '.lista', function () {
         let element = $(this)[0].parentElement.parentElement.parentElement.parentElement;
         let id = $(element).attr('idMusica')
-        idactual= Number(id);
+        idactual = Number(id);
         play(id)
     })
-    function siguiente(){
-        if(idactual===totalmusicas){
+    function siguiente() {
+        if (idactual === totalmusicas) {
             alert('ya no hay mas musicas para reproducir')
         }
-        else{
-        idactual=idactual+1; 
-        play(idactual.toString())
+        else {
+            idactual = idactual + 1;
+            play(idactual.toString())
         }
 
     }
-    function anterior(){
-        if(idactual===1){
+    function anterior() {
+        if (idactual === 1) {
             alert('no puede ir atras porque es la primera musica')
         }
-        else{
-        idactual=idactual-1; 
-        play(idactual.toString())
+        else {
+            idactual = idactual - 1;
+            play(idactual.toString())
         }
 
 
     }
     $(document).on('click', '.boton-siguiente', siguiente);
     $(document).on('click', '.boton-anterior', anterior);
-    function play(id){
+    function play(id) {
         $.ajax({
             url: 'musicaid.php',
             type: 'POST',
-            data: {id},
-            success: function(response){
+            data: { id },
+            success: function (response) {
                 let enlace = JSON.parse(response);
-                let template1 ='';
-                let template2 ='';
-                let template3 ='';
-                enlace.forEach(link =>{
+                let template1 = '';
+                let template2 = '';
+                let template3 = '';
+                enlace.forEach(link => {
                     template1 += `<audio controls class="audio-class" id="audio" autoplay>
                         <source src="https://drive.google.com/uc?export=download&amp;id=${link.ENLACE_M}" type="audio/mp3">
                     </audio>
